a0a9cb5802d72111bd0196c1ea3bbb43858fd2f9
Ross Harper <ross.harper@deliveroo.co.uk>
2022-12-05 17:11:42 +0000

    Day 4 part 2

--- 04/solution.go
+++ 04/solution.go
@@ -13,6 +13,10 @@ func main() {
 	result := solution("input.txt")
 
 	fmt.Println(strconv.Itoa(result))
+
+	result = solutionTwo("input.txt")
+
+	fmt.Println(strconv.Itoa(result))
 }
 
 func solution(filename string) int {
@@ -38,12 +42,58 @@ func solution(filename string) int {
 	return sum
 }
 
+func solutionTwo(filename string) int {
+	f, err := os.Open(filename)
+	utils.Check(err)
+
+	scanner := bufio.NewScanner(f)
+
+	sum := 0
+
+	for scanner.Scan() {
+		line := scanner.Text()
+
+		if line != "" {
+			if overlapped(line) {
+				sum++
+			}
+		}
+	}
+
+	f.Close()
+
+	return sum
+}
+
 func contained(input string) bool {
 	pairs := strings.Split(input, ",")
 	left := pairs[0]
 	right := pairs[1]
 
 	return containedWithin(left, right) || containedWithin(right, left)
+}
+
+func overlapped(input string) bool {
+	pairs := strings.Split(input, ",")
+	left := pairs[0]
+	right := pairs[1]
+	return arePairsOverlapped(left, right)
+}
+
+func arePairsOverlapped(s1 string, s2 string) bool {
+	left := strings.Split(s1, "-")
+	right := strings.Split(s2, "-")
+	leftStart, _ := strconv.Atoi(left[0])
+	leftEnd, _ := strconv.Atoi(left[1])
+	rightStart, _ := strconv.Atoi(right[0])
+	rightEnd, _ := strconv.Atoi(right[1])
+	return in(leftStart, rightStart, rightEnd) || in(leftEnd, rightStart, rightEnd) ||
+		in(rightStart, leftStart, leftEnd) || in(rightEnd, leftStart, leftEnd)
+}
+
+func in(v int, start int, end int) bool {
+
+	return v >= start && v <= end
 }
 
 func containedWithin(s1 string, s2 string) bool {
--- 04/solution_test.go
+++ 04/solution_test.go
@@ -44,3 +44,44 @@ func TestSolution(t *testing.T) {
 		})
 	}
 }
+
+func TestOverlapped(t *testing.T) {
+	var tests = []struct {
+		pairs string
+		want  bool
+	}{
+		{"2-4,6-8", false},
+		{"2-3,4-5", false},
+		{"5-7,7-9", true},
+		{"2-8,3-7", true},
+		{"6-6,4-6", true},
+		{"2-6,4-8", true},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.pairs, func(t *testing.T) {
+			ans := overlapped(tt.pairs)
+			if ans != tt.want {
+				t.Errorf("got %t, want %t", ans, tt.want)
+			}
+		})
+	}
+}
+
+func TestSolutionTwo(t *testing.T) {
+	var tests = []struct {
+		file string
+		want int
+	}{
+		{"test.txt", 4},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.file, func(t *testing.T) {
+			ans := solution(tt.file)
+			if ans != tt.want {
+				t.Errorf("got %d, want %d", ans, tt.want)
+			}
+		})
+	}
+}
